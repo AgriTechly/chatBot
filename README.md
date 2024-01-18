8cf483707747c268a52ae89ca94b53303e5aac55
unknown <a.e.k426rahmani@gmail.com>
2024-01-18 22:16:25 +0100

    main chat app

---
+++ README.md
@@ -0,0 +1,33 @@
+# Llama Chat 🦙
+
+This is a [Next.js](https://nextjs.org/) app that demonstrates how to build a chat UI using the [Llama 2](https://replicate.com/replicate/llama70b-v2-chat) language model and Replicate's [streaming API (private beta)](https://replicate.com/docs/streaming).
+
+Here's a demo:
+
+https://github.com/replicate/llama-chat/assets/14149230/9c6aaef3-4e60-4846-a2d2-f9575e155b70
+
+
+## Usage
+
+Install dependencies:
+
+```console
+npm install
+```
+
+Add your [Replicate API token](https://replicate.com/account#token) to `.env.local`:
+
+```
+REPLICATE_API_TOKEN=<your-token-here>
+```
+
+Run the development server:
+
+```console
+npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser.
+
+For detailed instructions on how to create and use this template, see [replicate.com/docs/get-started/nextjs](https://replicate.com/docs/get-started/nextjs)
+
---
+++ app/components/EmptyState.js
@@ -0,0 +1,73 @@
+export default function EmptyState({ setOpen, setPrompt }) {
+  return (
+    <div className="mt-12 sm:mt-24 space-y-6 text-gray-400 text-base mx-8 sm:mx-4 sm:text-2xl leading-12">
+      <p>
+        {" "}
+        AgriChat is the assistant chatbot for your{" "}
+        <button
+          className="prompt-button inline-flex items-center "
+          onClick={() => setOpen(true)}
+        >
+          AgriTechly Platform{" "}
+        </button>{" "}.
+      </p>
+      <p>
+        I can{" "}
+        <button
+          className="prompt-button"
+          onClick={() =>
+            setPrompt(
+              "Give some general agriculture advices."
+            )
+          }
+        >
+          give advice.
+        </button>
+        , have a deep understanding of the latest{" "}
+        <button
+          className="prompt-button"
+          onClick={() =>
+            setPrompt(" what are latest farming techniques, crop management strategies, and animal husbandry practices? ")
+          }
+        >
+         farming techniques,
+        </button>{" "}
+        crop management strategies, and animal husbandry practices
+        and willing to {" "}
+        <button
+          className="prompt-button"
+          onClick={() =>
+            setPrompt(
+              "give a general advice on how to improve the soil health of some farms in mena region."
+            )
+          }
+        >
+          help 
+        </button>
+        , farmers and agricultural businesses adopt environmentally friendly practices that promote soil health {" "}
+        <button
+          className="prompt-button"
+          onClick={() =>
+            setPrompt(
+              "give an advice on how to improve the yield of some farms in mena region."
+            )
+          }
+        >
+          biodiversity  
+        </button>
+         ,and efficient{" "}
+        <button
+          className="prompt-button"
+          onClick={() =>
+            setPrompt(
+              "give an advice on how to improve the water use of some farms in mena region."
+            )
+          }
+        >
+          water use.
+        </button>{" "}
+      </p>
+      <p>Send me a message.</p>
+    </div>
+  );
+}
